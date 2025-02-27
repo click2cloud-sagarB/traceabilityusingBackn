60c41abd190b675eef127f7a64d29c0f2dfb431a
Ubuntu <azureuser@backn-vm.xpoqoykl5oxurgrznokzmojrwc.bx.internal.cloudapp.net>
2025-02-27 08:53:32 +0000

    final running

--- backend/app.py
+++ backend/app.py
@@ -111,4 +111,5 @@ def track():
         return jsonify({"status": "error", "message": "Product not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
+
